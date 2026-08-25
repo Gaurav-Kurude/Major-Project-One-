f6c09873ac01a1661f890168b449c876a58f080d
Gaurav-Kurude <gauravkurude.sitmech@gmail.com>
2026-08-25 16:12:51 +0530

    Major Project One Backend

--- models/shopping.models.js
+++ models/shopping.models.js
@@ -1,50 +1,59 @@
 const mongoose = require("mongoose");
 
-const shoppingSchema = new mongoose.Schema({
-  productImage: {
-    type: String,
-    required: true,
-  },
-  productName: {
-    type: String,
-    required: true,
-  },
-  productPrice: {
-    type: Number,
-    required: true,
-  },
-  productDiscount: {
-    type: Number,
-    required: true,
-  },
-  productPriceBeforeDiscount: {
-    type: Number,
-    required: true,
-  },
-  productTitle: {
-    type: String,
-    required: true,
-  },
-  productRating: {
-    type: Number,
-    required: true,
-  },
-  productDescription: {
-    type: String,
-    required: true,
-  },
-  productDeliveryCharges: {
-    type: Number,
-    required: true,
-  },
+const shoppingSchema = new mongoose.Schema(
+  {
+    productImage: {
+      type: String,
+      required: true,
+    },
+    productName: {
+      type: String,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      required: true,
+    },
+    productDiscount: {
+      type: Number,
+      required: true,
+    },
+    productPriceBeforeDiscount: {
+      type: Number,
+      required: true,
+    },
+    productTitle: {
+      type: String,
+      required: true,
+    },
+    productRating: {
+      type: Number,
+      required: true,
+    },
+    productDescription: {
+      type: String,
+      required: true,
+    },
+    productDeliveryCharges: {
+      type: Number,
+      required: true,
+    },
     productCategory: {
-    type: [String],
-    required: true,
-    enum: ["Mens Clothing", "Women's Clothing"]
+      type: [String],
+      required: true,
+      enum: [
+        "Men's Clothing",
+        "Women's Clothing",
+        "Kid's Clothing",
+        "Electronics",
+        "Home",
+      ],
+    },
   },
-}, {
-  timestamps: true,
-});
+  {
+    timestamps: true,
+  },
+);
 
 const Shopping = mongoose.model("Shopping", shoppingSchema);
 module.exports = Shopping;
